cafecdf307199d3539fa386f0a2c3e9fcd8e1bfc
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-06-06 08:52:37 +0700

    feat : create doc readme.md file

--- app/Http/Controllers/Backend/SettingController.php
+++ app/Http/Controllers/Backend/SettingController.php
@@ -16,22 +16,26 @@ class SettingController extends Controller
     }
     public function update(Request $req)
     {
-        $data = setting::find(1);
-        $data->Nama_perusahaan = $req->Nama_perusahaan;
-        $data->tagline = $req->tagline;
-        $data->deskripsi = $req->deskripsi;
-        $data->email = $req->email;
-        $data->phone = $req->phone;
-        $data->url_website = $req->url_website ;
-        if($data->logo){
-            Storage::delete('public/'.$data->logo);
-        }
-        $data->logo = $req->file('logo') == "" ? $data->logo : $req->file('logo')->store('uploads/setting/logo', 'public');
-        if($data->Hero_cover){
-            Storage::delete('public/'.$data->Hero_cover);
-        }
-        $data->Hero_cover =  $req->file('Hero_Cover') == "" ? $data->Hero_cover :  $req->file('Hero_Cover')->store('uploads/setting/hero', 'public');
-        $data->save();
+        
+        setting::where(['id' => $id])->update([
+            ''
+        ]);
+        // $data = setting::find(1);
+        // $data->Nama_perusahaan = $req->Nama_perusahaan;
+        // $data->tagline = $req->tagline;
+        // $data->deskripsi = $req->deskripsi;
+        // $data->email = $req->email;
+        // $data->phone = $req->phone;
+        // $data->url_website = $req->url_website ;
+        // if($data->logo){
+        //     Storage::delete('public/'.$data->logo);
+        // }
+        // $data->logo = $req->file('logo') == "" ? $data->logo : $req->file('logo')->store('uploads/setting/logo', 'public');
+        // if($data->Hero_cover){
+        //     Storage::delete('public/'.$data->Hero_cover);
+        // }
+        // $data->Hero_cover =  $req->file('Hero_Cover') == "" ? $data->Hero_cover :  $req->file('Hero_Cover')->store('uploads/setting/hero', 'public');
+        // $data->save();
         return redirect()->back();
         // 'Nama_perusahaan', 'tagline', 'deskripsi', 'email', 'phone', 'url_website', 'logo', 'Hero_Cover'
     }
